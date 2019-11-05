f31a0e93469a709cac8cf431f26e74997c4a84e0
Jediael Fraser <jedfras@gmail.com>
2019-11-05 01:24:17 -0600

    init commit

---
+++ countingsort.py
@@ -0,0 +1,42 @@
+import sys
+from typing import List
+
+def counting_sort(arr: List[int]):
+    '''
+    Accepts a list of integers and returns the
+    sorted list, using the Counting-Sort algorithm.
+    Complexity: Big-Theta(k + n)
+    '''
+    M = max(arr) + 1
+    output = [0 for i in range(M)]
+    count = [0 for i in range(M)]
+    ans = [0 for i in arr]
+
+    # Update count with number of instances of each value
+    for i in arr:
+        count[i] += 1
+
+    # Update count s.t. each element stores sum of previous counts.
+    # This will reflect the position of each object in the output.
+    for i in range(M):
+        count[i] += count[i - 1]
+
+    for i in range(len(arr)):
+        output[count[arr[i]] - 1] = arr[i]
+        count[arr[i]] -= 1
+
+    for i in range(len(arr)):
+        ans[i] = output[i]
+
+    return ans
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: python countingsort.py [integer array]')
+        sys.exit()
+    a =[int(x) for x in sys.argv[1:]]
+    a_sorted = counting_sort(a)
+    print(a_sorted)
+
+if __name__ == '__main__':
+    main()
